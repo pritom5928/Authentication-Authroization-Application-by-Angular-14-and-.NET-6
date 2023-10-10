b0debca7d59241d1d1fd37e7b45288b32850bf37
pritom5928 <pritompal.softifybd@gmail.com>
2023-10-10 21:00:49 +0600

    dssd

--- AngularAuthAPI/AngularAuthAPI/Controllers/UserController.cs
+++ AngularAuthAPI/AngularAuthAPI/Controllers/UserController.cs
@@ -3,6 +3,8 @@ using AngularAuthAPI.Helpers;
 using AngularAuthAPI.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace AngularAuthAPI.Controllers
 {
@@ -37,6 +39,23 @@ namespace AngularAuthAPI.Controllers
             if (userObj == null)
                 return BadRequest();
 
+
+            //check username
+            if(await CheckUserNameExistsAsync(userObj.UserName))
+                return BadRequest(new { Message = "Username already exists!" });
+
+
+            //check email
+            if (await CheckEmailExistsAsync(userObj.Email))
+                return BadRequest(new { Message = "Email already exists!" });
+
+
+            //check password strength
+            var passwordInvalidationMsg = CheckPasswordStrength(userObj.Password);
+            if (!string.IsNullOrWhiteSpace(passwordInvalidationMsg))
+                return BadRequest(new { Message = passwordInvalidationMsg });
+
+
             userObj.Password = PasswordHasher.HashPassword(userObj.Password);
             userObj.Role = "User";
             userObj.Token = "";
@@ -47,5 +66,36 @@ namespace AngularAuthAPI.Controllers
             return Ok(new { Message = "Registration success!" });
         }
 
+        private async Task<bool> CheckUserNameExistsAsync(string userName)
+        {
+           return await _db.Users.AnyAsync(u => u.UserName == userName);
+        }
+
+        private async Task<bool> CheckEmailExistsAsync(string email)
+        {
+            return await _db.Users.AnyAsync(u => u.Email == email);
+        }
+
+        private string CheckPasswordStrength(string password)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            if(password.Length < 8)
+                sb.Append("Minimum password length should be 8 "+ Environment.NewLine);
+
+            //if (!(Regex.IsMatch(password, "[a-z]") && Regex.IsMatch(password, "[A-Z]") && Regex.IsMatch(password, "[0-9]")))
+            //    sb.Append("Password should be AlphaNumeric" + Environment.NewLine);
+
+            if (!(Regex.IsMatch(password, "[a-z A-Z 0-9]+")))
+                sb.Append("Password should be AlphaNumeric" + Environment.NewLine);
+
+            
+
+            if (!(Regex.IsMatch(password, "[!@#$%^&*()_+{}\\[\\]:;<>,.?~|\\\\]")))
+                sb.Append("Password should be contain special characters" + Environment.NewLine);
+
+
+            return sb.ToString();
+        }
     }
 }
