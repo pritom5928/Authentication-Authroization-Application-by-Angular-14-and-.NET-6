b69d9fe781be09a1901c492af0f4f2102bbda1b6
pritom5928 <pritompal.softifybd@gmail.com>
2023-11-06 00:55:52 +0600

    email template added for forgot password

--- AngularAuthAPI/AngularAuthAPI/UtilityService/EmailService.cs
+++ AngularAuthAPI/AngularAuthAPI/UtilityService/EmailService.cs
@@ -1,13 +1,46 @@
 ﻿using AngularAuthAPI.Models;
+using MailKit.Net.Smtp;
+using MimeKit;
 
 namespace AngularAuthAPI.UtilityService
 {
     public class EmailService : IEmailService
     {
+        private IConfiguration _configuration;
+        public EmailService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
 
         public void SendMail(EmailModel emailModel)
         {
-           
+            var emailMessage = new MimeMessage();
+            var from = _configuration["EmailSettings:From"];
+            emailMessage.From.Add(new MailboxAddress("Let's code", from));
+            emailMessage.To.Add(new MailboxAddress(emailModel.To, emailModel.To));
+            emailMessage.Subject = emailModel.Subject;
+            emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html)
+            {
+                Text = string.Format(emailModel.Content)
+            };
+
+            using(var client = new SmtpClient())
+            {
+                try
+                {
+                    client.Connect(_configuration["EmailSettings:SmtpServer"], int.Parse(_configuration["EmailSettings:Port"]), true);
+                    client.Authenticate(_configuration["EmailSettings:Username"], _configuration["EmailSettings:Password"]);
+                }
+                catch (Exception ex)
+                {
+                    throw;
+                }
+                finally
+                {
+                    client.Disconnect(true);
+                    client.Dispose();
+                }
+            }
         }
     }
 }
